7651557232c887000b043929537d686448165590
tareqsifat <mdtareq.me@gmail.com>
2021-04-24 21:49:14 +0600

    --- Auto-Git Commit ---

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -18,7 +20,21 @@ class UserSeeder extends Seeder
         User::truncate(); 
 
         $user = new User();
-        $user -> role_id() = 1;
+
+        $user -> role_id = 4;
+        $user-> first_name = 'Mr super';
+        $user-> last_name = 'user';
+        $user-> username = 'super_user';
+        // $user-> photo = 
+        $user-> phone = '987654321';
+        $user-> email = 'superuser@gwebsite.com';
+        $user-> password = Hash::make();
+        $user-> creator = Auth::user()-> id;
+        $user-> slug = 'super_user';
+        $user-> created_at = Carbon::now() -> toDateString();
+        $user-> save();
+
+        
 
         
         
