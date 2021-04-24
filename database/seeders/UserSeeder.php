e569ab70ef54d99956ecb259d9759db963bb7bd7
tareqsifat <mdtareq.me@gmail.com>
2021-04-24 18:15:39 +0600

    --- Auto-Git Commit ---

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class userSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        user::truncate(); 
+
+        $user = new user();
+        $
+        
+    }
+}
